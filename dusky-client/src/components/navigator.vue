f789c50702b896ef44b8ea7f58785a07cbca8078
kdeng <kdeng@hillinsight.com>
2018-07-05 10:53:24 +0800

    使用新的脚手架重做项目

---
+++ dusky-client/src/components/navigator.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="navigator">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Welcome To Dusky Home!</a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav m-auto">
+                        <router-link to="home" tag="li" class="nav-link active">
+                            Home
+                        </router-link>
+
+                        <router-link to="tools" tag="li" class="nav-link">
+                            Tools
+                        </router-link>
+
+                        <router-link to="logs" tag="li" class="nav-link">
+                            Logs
+                        </router-link>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="sass" rel="stylesheet/scss" scoped>
+
+</style>
+
+
---
+++ dusky-client/src/router/index.js
@@ -0,0 +1,34 @@
+Vue.use(VueRouter);
+
+const Home = resolve => require(['@components/home.vue'], resolve);
+
+const router = new VueRouter({
+    mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            return {
+                selector: to.hash
+            }
+        } else {
+            return {x: 0, y: 0}
+        }     
+    },
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home 
+        }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = `Dusky-${to.name}`;
+    next();
+});
+
+export default router;
