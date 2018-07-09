3ac5c5749c29d7e1f672867637d94885eae45b9f
kdeng <kdeng@hillinsight.com>
2018-07-09 17:23:15 +0800

    提交工具箱界面

--- dusky-client/src/components/tools/index.vue
+++ dusky-client/src/components/tools/index.vue
@@ -1,6 +1,18 @@
 <template>
-    <div id="tools">
-        this is empty tools page!
+    <div id="tools" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="my-4">工具箱合集</h4>
+            </div>
+            <div v-for="(tool, index) in toolList" 
+                :key="index"
+                class="col-6 col-sm-4 col-md-3 col-lg-2 tool-container"> 
+
+                <div class="tool-item">
+                    {{tool}}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,12 +20,23 @@
 export default {
     data() {
         return {
-
+            toolList: ['too1 1', 'too1 2', 'too1 3', 'too1 4', 'too1 5', 'too1 6', 'too1 7', 'too1 8', 'too1 9', 'too1 10']
         };
     }
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.my-4 {
+    color: cadetblue;
+}
+.tool-container {
+    padding: 5px;
 
+    .tool-item {
+        border: 1px dotted blue;
+        cursor: pointer;
+        color: cadetblue;
+    }
+}
 </style>
