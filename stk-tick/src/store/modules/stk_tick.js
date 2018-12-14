410199a2d77e5e4615b585e857283170cf75e2c3
kdeng <369069851@qq.com>
2018-12-14 22:54:19 +0800

    123

--- stk-tick/src/store/modules/stk_tick.js
+++ stk-tick/src/store/modules/stk_tick.js
@@ -14,7 +14,7 @@ const state = {
   date: "",
   ts_code: "",
   dataArr: [],
-  timeFilter: timeFilterArray[0]  
+  time_filter: timeFilterArray[0]
 };
 
 const mutations = {
@@ -38,24 +38,52 @@ const actions = {
       params: {
         ts_code: state.ts_code,
         date: state.date,
-        timeFilter: state.timeFilter
+        time_filter: state.time_filter
       }
     })
-    .then((response) => {
-      const arr = response.data.result.split('\r\n');
-      const dataArr = [];
+      .then((response) => {
+        const arr = response.data.result.split('\r\n');
+        const dataArr = [];
 
-      arr.forEach((item) => {
-        const obj = item.split(',');
-        dataArr.push({
-
+        arr.forEach((item) => {
+          if (!item || !item.split) {
+            return;
+          }
+          const obj = item.split(',');
+          dataArr.push({
+            time: obj[2].split(' ')[1],
+            price: obj[3],
+            count: obj[4],
+            sum: obj[5],
+            amount: obj[6],
+            direction: obj[7],
+            bp1: obj[8],
+            bp2: obj[9],
+            ob3: obj[10],
+            bp4: obj[11],
+            bp5: obj[12],
+            sp1: obj[13],
+            sp2: obj[14],
+            sp3: obj[15],
+            sp4: obj[16],
+            sp5: obj[17],
+            bq1: obj[18],
+            bq2: obj[19],
+            bq3: obj[20],
+            bq4: obj[21],
+            bq5: obj[22],
+            sq1: obj[23],
+            sq2: obj[24],
+            sq3: obj[25],
+            sq4: obj[26],
+            sq5: obj[27]
+          });
         });
+        commit('updateState', { dataArr });
+      })
+      .catch((error) => {
+        console.log(`getDatas err: ${error}`);
       });
-      commit('updateState', { dataArr });
-    })
-    .catch((error) => {
-      console.log(`getDatas err: ${error}`);
-    });
   },
   getTradeDate({ commit, state }, { date, type }) {
     axios.get('https://www.dusky.xyz/api/stock/trade-date', {
@@ -64,12 +92,12 @@ const actions = {
         type
       }
     })
-    .then((response) => {
-      commit('updateState', { date: response.data.result });
-    })
-    .catch((error) => {
-      console.log(`getTradeDate err: ${error}`);
-    }); 
+      .then((response) => {
+        commit('updateState', { date: response.data.result });
+      })
+      .catch((error) => {
+        console.log(`getTradeDate err: ${error}`);
+      });
   }
 }
 
